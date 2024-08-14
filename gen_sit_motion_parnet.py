8f80660c72a32b8689a830a0031856afa1571357
zlicheng <zlicheng@sjtu.edu.cn>
2024-08-14 17:40:49 +0800

    init

--- gen_sit_motion_parnet.py
+++ gen_sit_motion_parnet.py
@@ -13,14 +13,35 @@ def trans_to_center(obj_value):
     return obj_value
 
 
+def aug_mesh(a, obj_value):
+    return obj_value
+
+
+def run_cmd(args, i, a, value_final, key, tmp_path):
+    value_final = trans_to_center(value_final)
+    tmp_dict = {key: value_final}
+
+    with open(tmp_path, 'w') as f:
+        json.dump(tmp_dict, f, indent=4)
+
+    viz = "" if args.viz else "--headless"
+
+    print(f"\033[91mbegin {i}/{len_objs}: {a}\033[0m")
+    os.system(
+        f"CUDA_VISIBLE_DEVICES={args.gpu_id} python3 unihsi/run.py --obj_file {tmp_path} --task {args.task} --save_root {args.save_root} {viz} --test --num_envs 1 --cfg_env unihsi/data/cfg/humanoid_unified_interaction_scene_0.yaml --cfg_train unihsi/data/cfg/train/rlg/amp_humanoid_task_deep_layer_2we.yaml --motion_file motion_clips/chair_mo.npy --checkpoint checkpoints/Humanoid.pth"
+    )
+    print(f"\033[91mend {i}/{len_objs}: {a}\033[0m")
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-j', '--json', required=True)
     parser.add_argument('-t', '--tmp', type=str, required=True)
     parser.add_argument('-g', '--gpu_id', type=str, required=True)
     parser.add_argument('-n', '--num', type=int, default=5)
+    parser.add_argument('-an', '--aug_num', type=int, default=10)
     parser.add_argument('-s', '--save_root', type=str, required=True)
-    parser.add_argument('--task', type=str, required=True, choices=['UniHSI_PartNet', 'UniHSI_PartNet_aug'])
+    parser.add_argument('--task', type=str, required=True, choices=['UniHSI_PartNet', 'UniHSI_PartNet_AUG'])
     parser.add_argument('--viz', action='store_true', default=False)
     parser.add_argument('--actions', default=["chair", "bed"])
     args = parser.parse_args()
@@ -40,16 +61,11 @@ if __name__ == '__main__':
 
         value["obj"]["000"]["count"] = args.num
 
-        value = trans_to_center(value)
-        tmp_dict = {key: value}
-
-        with open(tmp_path, 'w') as f:
-            json.dump(tmp_dict, f, indent=4)
-
-        viz = "" if args.viz else "--headless"
-
-        print(f"\033[91mbegin {i}/{len_objs}\033[0m")
-        os.system(
-            f"CUDA_VISIBLE_DEVICES={args.gpu_id} python3 unihsi/run.py --obj_file {tmp_path} --task {args.task} --save_root {args.save_root} {viz} --test --num_envs 1 --cfg_env unihsi/data/cfg/humanoid_unified_interaction_scene_0.yaml --cfg_train unihsi/data/cfg/train/rlg/amp_humanoid_task_deep_layer_2we.yaml --motion_file motion_clips/chair_mo.npy --checkpoint checkpoints/Humanoid.pth"
-        )
-        print(f"\033[91mend {i}/{len_objs}\033[0m")
+        if args.task == "UniHSI_PartNet":
+            run_cmd(args, i, 0, value_final, key, tmp_path)
+        elif args.task == "UniHSI_PartNet_AUG":
+            for a in range(args.aug_num):
+                value_final = aug_mesh(a, value)
+                run_cmd(args, i, a, value_final, key, tmp_path)
+        else:
+            raise ValueError()
